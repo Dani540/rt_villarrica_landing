56e660b02e17d4a5e6b09507ac7dad016a492c2b
= <=>
2025-02-16 15:08:19 -0300

    Update project title and configure Vercel adapter with integrations

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,11 +4,11 @@ import { defineConfig } from 'astro/config';
 import icon from 'astro-icon';
 
 import vue from '@astrojs/vue';
-import vercel from "@astrojs/vercel";
+import vercel from '@astrojs/vercel';
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [icon(), vue()],
   output: 'server',
-  adapter: vercel()
+  adapter: vercel(),
+  integrations: [icon(), vue()]
 });
