bf233ffa83e572c0dd2a528752a2cb6550c5e75f
= <=>
2025-02-16 14:47:49 -0300

    Add FormCheckbox component for reusable checkbox input with two-way binding

---
+++ src/components/vue/Inscription/FormCheckbox.vue
@@ -0,0 +1,19 @@
+<script setup lang="ts">
+const props = defineProps({
+  id: { type: String, required: true },
+  label: { type: String, required: true },
+  modelValue: { type: Boolean, default: false }
+});
+</script>
+
+<template>
+  <div class="checkbox-item">
+    <input
+        type="checkbox"
+        :id="id"
+        :checked="modelValue"
+        @change="$emit('update:modelValue', $event.target.checked)"
+    />
+    <label :for="id">{{ label }}</label>
+  </div>
+</template>
